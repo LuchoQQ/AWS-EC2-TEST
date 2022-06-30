a85a3c00d5d9387035515fe0136f374f5d0a4d29
luchoqq <lucianosanchez125@hotmail.com>
2022-06-30 01:04:12 -0300

    updated

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-const app = express()
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
 
 app.listen(3000)
 
